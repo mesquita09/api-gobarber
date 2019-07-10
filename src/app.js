cc923150fbfefd91195f4da80b39d5bae107cd3f
Adelson Mesquita <adelson@MacBook-Pro-de-Adelson.local>
2019-07-10 17:29:49 -0300

    chore: git sync

---
+++ src/app.js
@@ -0,0 +1,25 @@
+//const express = require("express");
+//const routes = require("./routes");
+// sintaxe antiga, instalando o sucrase, permite usar a nova sintaxe (abaixo)
+
+import express from "express";
+import routes from "./routes";
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+  middlewares() {
+    this.server.use(express.json());
+  }
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+//module.exports = new App().server;
+// sintaxe antiga, instalando o sucrase, permite usar a nova sintaxe (abaixo)
+export default new App().server;
