318cb36bfca2e0b3d70331377c23097517db7899
Adelson Mesquita <adelson@MacBook-Pro-de-Adelson.local>
2019-07-10 19:07:29 -0300

    chore: sequelize configuration

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-//const express = require("express");
-//const routes = require("./routes");
+// const express = require("express");
+// const routes = require("./routes");
 // sintaxe antiga, instalando o sucrase, permite usar a nova sintaxe (abaixo)
 
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 class App {
   constructor() {
@@ -12,14 +12,16 @@ class App {
     this.middlewares();
     this.routes();
   }
+
   middlewares() {
     this.server.use(express.json());
   }
+
   routes() {
     this.server.use(routes);
   }
 }
 
-//module.exports = new App().server;
+// module.exports = new App().server;
 // sintaxe antiga, instalando o sucrase, permite usar a nova sintaxe (abaixo)
 export default new App().server;
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialects: 'postgress',
+  host: 'localhost',
+  username: 'postgress',
+  password: '14gun420',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
