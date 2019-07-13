473427027e522a58572ac08aa7b74e4df5b279c2
Adelson Mesquita <adelson@MacBook-Pro-de-Adelson.local>
2019-07-12 23:27:51 -0300

    model loader creation

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello world' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego',
+    email: 'teste@teste.com',
+    password_hash: '123123132',
+    provider: false,
+    // precisei declarar o provider para funcionar
+  });
+
+  return res.json(user);
+});
 
 export default routes;
