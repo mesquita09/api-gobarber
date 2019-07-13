9ee7f6f77bafd5e1c96a1d37e6a84faa71593312
Adelson Mesquita <adelson@MacBook-Pro-de-Adelson.local>
2019-07-13 11:56:04 -0300

    feat: user registration

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'joao',
-    email: 'teste@teste.com',
-    password_hash: '123123132',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
