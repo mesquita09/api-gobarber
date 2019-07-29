84c765fafd714501f5180ebffa254c0b6a45fb54
mesquita09 <mesquita.adelson@gmail.com>
2019-07-29 18:15:55 -0300

    feat: nodemailer initial config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '213e40e7ec1be0',
+    pass: '8ae3a8b72af4e0',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
